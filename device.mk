b07cae17814b8819092438479410db9024f1d9b0
lahaina <nattolecats@gmail.com>
2024-12-18 17:42:13 +0900

    cancunf: overlay: Import regulatory info for ymobile variant

--- device.mk
+++ device.mk
@@ -315,6 +315,7 @@ PRODUCT_PACKAGES += \
     RegulatoryOverlayXT2343-4 \
     RegulatoryOverlayXT2343-5 \
     RegulatoryOverlayXT2431-1 \
+    RegulatoryOverlayXT2431-2 \
     RegulatoryOverlayXT2431-3
 
 # Permissions
